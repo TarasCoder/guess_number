aa10a06630b9ccd8e56161c2c7d45f26ec131264
kaskad <kaskad@bigmir.net>
2021-07-19 18:25:24 +0300

    adding game logic

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const btn = document.querySelector("#btn");
+const reset = document.querySelector("#reset");
 const input = document.querySelector("#input");
-btn.addEventListener("click", function(ev) {
-    ev.preventDefault();
-    console.log(input.value);
-    input.value = "";
-})
+const yourScore = document.querySelector("#yourScore");
+const compScore = document.querySelector("#compScore");
+
+let yourScoreVal = 0;
+let compScoreVal = 0;
+
+yourScore.innerText = yourScoreVal;
+compScore.innerText = compScoreVal;
+
+btn.addEventListener("click", function (ev) {
+  ev.preventDefault();
+  let random = Math.floor(Math.random() * 5 + 1);
+  console.log("Random number this time is: " + random);
+  check(input.value, random);
+  input.value = "";
+});
+reset.addEventListener("click", function () {
+  yourScoreVal = 0;
+  compScoreVal = 0;
+  yourScore.innerText = yourScoreVal;
+  compScore.innerText = compScoreVal;
+});
+
+function check(num, random) {
+  // parseInt(num);
+  if (parseInt(num) == random) {
+    alert("You win!!! Congratulations!!!");
+    yourScoreVal++;
+    yourScore.innerText = yourScoreVal;
+  } else {
+    alert("Sorry no - let's try again!");
+
+    compScoreVal++;
+    compScore.innerText = compScoreVal;
+  }
+}
